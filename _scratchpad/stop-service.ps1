801c5e5ccbc4aab7367b8e48595adee009aeb805
rgracia <rgracia@netchexonline.com>
2020-04-08 12:02:14 -0500

    minor change

--- _scratchpad/stop-service.ps1
+++ _scratchpad/stop-service.ps1
@@ -4,7 +4,7 @@ while ($isRunning)
     $ErrorActionPreference = 'SilentlyContinue'
     $ApiProcesses = $ApiProcesses | ForEach-Object { try { Get-Process -id $_.id } catch{} }
 
-    if ($null -eq $JmeterProcesses)
+    if ($null -eq $ApiProcesses)
     {
         $ErrorActionPreference = 'Continue'
         $isRunning = $false
